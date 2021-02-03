69f122ebed9a1f71257b8253ccd44659148fa6f3
vuquanghuy2001 <59812614+vuquanghuy2001@users.noreply.github.com>
2021-02-03 22:44:00 +0700

    Updare: View

--- MedialCare/MedialCare/Areas/Admin/Controllers/UsersController.cs
+++ MedialCare/MedialCare/Areas/Admin/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MedialCare.Models.EF;
 using MedialCare.Models.Entities;
+using MedialCare.Models.Catalog;
 
 namespace MedialCare.Areas.Admin.Controllers
 {
@@ -14,16 +15,41 @@ namespace MedialCare.Areas.Admin.Controllers
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _context;
-
+        public int PageSize = 5;
         public UsersController(ApplicationDbContext context)
         {
             _context = context;
         }
 
         // GET: Admin/Users
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string keySearch, int page=1)
         {
-            return View(await _context.Users.ToListAsync());
+            var query = from u in _context.Users select new { u };
+
+            if(keySearch != null)
+            {
+                query = query.Where(x => x.u.FirstName.Contains(keySearch));
+            }
+            var info = new ListViewModel
+            {
+                User = await query.Select(x => new User
+                {
+                    ID = x.u.ID,
+                    FirstName = x.u.LastName,
+                    Email = x.u.Email,
+                    Gender = x.u.Gender,
+                    Address = x.u.Address,
+                    Phone = x.u.Phone,
+                    Salary = x.u.Salary,
+                }).OrderBy(x => x.ID).Skip((page - 1) * PageSize).Take(PageSize).ToListAsync(),
+                PagingModel = new PagingModel
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = _context.Users.Count()
+                }
+            };
+            return View(info);
         }
 
         // GET: Admin/Users/Details/5
@@ -82,6 +108,25 @@ namespace MedialCare.Areas.Admin.Controllers
             return View(user);
         }
 
+
+        // GET: Admin/Users/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            ViewData["ID"] = new SelectList(_context.Users, "ID", "Name", user.ID);
+            return View(user);
+        }
+
         // POST: Admin/Users/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -114,24 +159,7 @@ namespace MedialCare.Areas.Admin.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(user);
-        }
-
-        // GET: Admin/Users/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
+            ViewData["ID"] = new SelectList(_context.Users, "ID", "Level", user.ID);
             return View(user);
         }
 
@@ -143,6 +171,7 @@ namespace MedialCare.Areas.Admin.Controllers
             var user = await _context.Users.FindAsync(id);
             _context.Users.Remove(user);
             await _context.SaveChangesAsync();
+            ViewData["ID"] = new SelectList(_context.Users, "ID", "Level", user.ID);
             return RedirectToAction(nameof(Index));
         }
 
