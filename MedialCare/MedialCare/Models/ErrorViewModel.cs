50a31e582d5d556c307d016365539e647e9aa1fd
THANH TU TRUONG <truongthanhtu.it.1998@gmail.com>
2021-02-03 14:24:59 +0700

    Create Project Sm3

---
+++ MedialCare/MedialCare/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MedialCare.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
