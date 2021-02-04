adf56fe0160f26f2a28d03f138d544dfc745f387
Tuan Pham <58241974+tuanp20@users.noreply.github.com>
2021-02-04 19:54:25 +0700

    update: UI Employee

---
+++ MedialCare/MedialCare/Controllers/FeedBackController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedialCare.Controllers
+{
+    public class FeedBackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
